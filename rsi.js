3e6572b8227f8bba5354c12c80c6439e8ce608c3
sdlab1 <59804621+sdlab1@users.noreply.github.com>
2024-07-21 16:53:15 +0300

    Update rsi.js

--- rsi.js
+++ rsi.js
@@ -1,3 +1,22 @@
+function rma(values, period){
+    const alpha = 1 / period;
+    const rmaArray = [];
+    rmaArray.push(values.slice(0, period).reduce((acc, val) => acc + val, 0) / period);
+    for (let i = period; i < values.length; i++)
+      rmaArray.push(alpha * values[i] + (1 - alpha) * rmaArray[rmaArray.length-1]);
+    return rmaArray;
+  }
+// in basic.js:
+export function rma(arr, period) { //running moving average
+    const arrout = [];
+	let tmp = 0;
+    for(let i = 0; i < period; i++)
+        tmp += arr[i];
+    arrout[0] = tmp / period; //1st sma value
+    for(let i = period, j = 1, alpha = 1 / period; i < arr.length; i++, j++)
+        arrout[j] = arr[i] * alpha + (1 - alpha) * arrout[j-1];
+    return arrout;
+}
 function mathmx(arg1, arg2) { 
   //since we have only 2 args and 1 is for sure greater
   //and both are numbers no need for Math.max. This is the way.
@@ -10,23 +29,13 @@ function rsi(ohlcv, period = 14){
   const down = [];
 
   // Calculate the gains and losses
-  for (let i = 1; i < ohlcv.length; i++) { //branchless; Number.MIN_VALUE as zero
+  for (let i = 1; i < ohlcv.length; i++) { //Number.MIN_VALUE as zero
     const change = ohlcv[i].close - ohlcv[i - 1].close;
-    const upValue = mathmx(change, Number.MIN_VALUE); // Equivalent to if (change >= 0) { change } else { Number.MIN_VALUE }
-    const downValue = mathmx(-change, Number.MIN_VALUE); // Equivalent to if (change < 0) { -change } else { Number.MIN_VALUE }
+    const upValue = mathmx(change, Number.MIN_VALUE);
+    const downValue = mathmx(-change, Number.MIN_VALUE);
     up.push(upValue);
     down.push(downValue);
   }
-  
-  function rma(values, period){
-    const alpha = 1 / period;
-    const rmaArray = [];
-    rmaArray.push(values.slice(0, period).reduce((acc, val) => acc + val, 0) / period);
-    for (let i = period; i < values.length; i++)
-      rmaArray.push(alpha * values[i] + (1 - alpha) * rmaArray[rmaArray.length-1]);
-    return rmaArray;
-  }
-
   // Calculate the RMA of gains and losses
   const upRma = rma(up, period);
   const downRma = rma(down, period);
